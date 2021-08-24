1b5cdd64bc4ef58f5ceaa192a4ed985e6bdf758d
Samuel Sülzle <samuel.suelzle@plan-b-gmbh.com>
2021-08-24 10:48:55 +0200

    Add project files.

---
+++ XMLRegexTest/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace XMLRegexTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
