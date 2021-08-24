3ca5f8921285dec5dbc4f9d04773fb1410cf9fc9
Samuel Sülzle <samuel.suelzle@plan-b-gmbh.com>
2021-08-24 11:10:36 +0200

    Add seperator regex

--- XMLRegexTest/Program.cs
+++ XMLRegexTest/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace XMLRegexTest
 {
-    class Program
+    /// <summary>
+    /// The Program class.
+    /// </summary>
+    public class Program
     {
+        /// <summary>
+        /// The main method.
+        /// </summary>
+        /// <param name="args">string args.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string body = string.Empty;
+
+            // Logic with added seperators.
+            string xmlSteps = "<steps><step>GET http://url <BR/> body: <?xml version=\"1.0\" encoding=\"utf - 8\"?><Envelope xmlns=\"http://www.w3.org/2003/05/soap-envelope\"><Body></Body></Envelope></step></steps>";
+            Regex regex = new Regex(@"(?<=\<\!\-\-start\-\-\>)[^\)]+(?=\<\!\-\-end\-\-\>)");
+            if (regex.Match(xmlSteps).Success == true)
+            {
+                body = regex.Match(xmlSteps).Value;
+            }
+            else
+            {
+                Console.WriteLine("Do VSTS TestExecutor Logic here.");
+            }
+
+            Console.ReadKey();
         }
     }
 }
