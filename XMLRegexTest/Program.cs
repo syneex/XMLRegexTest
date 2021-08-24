a63b491390a68ef3b796d0536e7d2a5b25e26e69
Samuel Sülzle <samuel.suelzle@plan-b-gmbh.com>
2021-08-24 11:50:56 +0200

    added regex without seperators

--- XMLRegexTest/Program.cs
+++ XMLRegexTest/Program.cs
@@ -14,14 +14,27 @@ namespace XMLRegexTest
         /// <param name="args">string args.</param>
         static void Main(string[] args)
         {
+            string xmlSteps = "<steps><step>GET http://url <BR/> body: <?xml version=\"1.0\" encoding=\"utf - 8\"?><Envelope xmlns=\"http://www.w3.org/2003/05/soap-envelope\"><Body></Body></Envelope> </step></steps>";
             string body = string.Empty;
 
             // Logic with added seperators.
-            string xmlSteps = "<steps><step>GET http://url <BR/> body: <?xml version=\"1.0\" encoding=\"utf - 8\"?><Envelope xmlns=\"http://www.w3.org/2003/05/soap-envelope\"><Body></Body></Envelope></step></steps>";
-            Regex regex = new Regex(@"(?<=\<\!\-\-start\-\-\>)[^\)]+(?=\<\!\-\-end\-\-\>)");
-            if (regex.Match(xmlSteps).Success == true)
+            Regex seperatorRegex = new Regex(@"(?<=\<\!\-\-start\-\-\>)[^\)]+(?=\<\!\-\-end\-\-\>)");
+            if (seperatorRegex.Match(xmlSteps).Success == true)
             {
-                body = regex.Match(xmlSteps).Value;
+                body = seperatorRegex.Match(xmlSteps).Value;
+                Console.WriteLine("Body" + body);
+            }
+            else
+            {
+                Console.WriteLine("Do VSTS TestExecutor Logic here.");
+            }
+
+            // Logic without added seperators.
+            Regex withoutSeperatorRegex = new Regex(@"\s+body\s*[:=]\s*(.*)([]}>""] | (<\/step>))");
+            if (withoutSeperatorRegex.Match(xmlSteps).Success == true)
+            {
+                body = withoutSeperatorRegex.Match(xmlSteps).Groups[1].Value;
+                Console.WriteLine("Body" + body);
             }
             else
             {
